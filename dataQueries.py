e22af0ce38ff43d59e3b54813880788215e6e688
serje3 <serjerin322@gmail.com>
2020-09-01 15:01:59 +0300

    Improved issue with psycopg2

--- dataQueries.py
+++ dataQueries.py
@@ -40,13 +40,13 @@ class ManageDB:
 
     def insert(self, arr):
         cursor = self.conn.cursor()
-        cursor.executemany("""INSERT INTO songs(song,guild) VALUES(?,?)""", arr)
+        cursor.executemany("""INSERT INTO songs(song,guild) VALUES(%s,%s)""", arr)
         self.conn.commit()
         cursor.close()
 
     def select(self, guild_id):
         cursor = self.conn.cursor()
-        cursor.execute("""SELECT * FROM songs WHERE guild=:guild_id""", {'guild_id': guild_id})
+        cursor.execute("""SELECT * FROM songs WHERE guild=%(guild_id)s""", {'guild_id': guild_id})
         result = cursor.fetchall()
         cursor.close()
         return result
@@ -54,7 +54,7 @@ class ManageDB:
     def drop(self, id_arr, guild):
         cursor = self.conn.cursor()
         for _id in id_arr:
-            cursor.execute("""DELETE FROM songs WHERE id=:id and guild=:guild_id""", {'id': _id, 'guild_id': guild})
+            cursor.execute("""DELETE FROM songs WHERE id= %(id)s and guild=%(guild_id)s""", {'id': _id, 'guild_id': guild})
             self.conn.commit()
         cursor.close()
 
