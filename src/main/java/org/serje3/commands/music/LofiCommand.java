2c87ce3834da7d7b428b95f2b8b7294576f8f741
serje3 <serjerin322@gmail.com>
2024-02-13 03:49:15 +0300

    Added lofi command

--- src/main/java/org/serje3/commands/music/LofiCommand.java
+++ src/main/java/org/serje3/commands/music/LofiCommand.java
@@ -3,6 +3,7 @@ package org.serje3.commands.music;
 import dev.arbjerg.lavalink.client.LavalinkClient;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import org.serje3.commands.music.queue.QueueCommand;
 import org.serje3.meta.abs.Command;
 import org.serje3.utils.VoiceHelper;
 
@@ -27,6 +28,6 @@ public class LofiCommand extends Command {
 
         final String identifier = "https://www.youtube.com/watch?v=rUxyKA_-grg";
         final long guildId = guild.getIdLong();
-        new PlayCommand().play(client, event, guildId, identifier);
+        new QueueCommand().play(client, event, guildId, identifier);
     }
 }
